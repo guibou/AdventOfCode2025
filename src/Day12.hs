2171dd75ae2c0ec95a2ef172c7cfbcba7abbfb79
Guillaume Bouchard <guillaum.bouchard@gmail.com>
2026-01-04 18:30:32 +0100

    Parser for Day12

--- src/Day12.hs
+++ src/Day12.hs
@@ -1,10 +1,41 @@
 module Day12 where
 
 import Utils
+import qualified Data.Text as Text
+import qualified Data.Set as Set
+import Control.Applicative.Combinators (sepBy)
+import Text.Megaparsec (label, sepBy1)
 
 fileContent = parseContent $(getFile)
 
-parseContent = undefined
+parseContent t = do
+  let items = Text.splitOn "\n\n" t
+      shapes = init items
+      regions = last items
+  (map parseShape shapes, unsafeParse parseRegions regions)
+
+parseRegions = some (parseRegion <* "\n")
+
+parseRegion = do
+  width <- parseNumber
+  "x"
+  length <- parseNumber
+  ": "
+  quantities <- some parseNumber
+
+  pure Region{..}
+
+data Region = Region {
+  width :: Int,
+  length :: Int,
+  quantities :: [Int]
+} deriving (Show)
+
+parseShape t = Set.fromList $ do
+  (lineNo, l) <- zip [0..] (drop 1 $ Text.lines t)
+  (colNo, c) <- zip [0..] (Text.unpack l)
+  guard $ c == '#'
+  pure $ V2 lineNo colNo
 
 -- * Generics
 
